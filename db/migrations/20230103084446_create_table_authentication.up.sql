d253eaa8635931bdfefbda873bd5d467c53cbc86
elSyarif <id.syarif@gmail.com>
2023-01-03 16:00:43 +0700

    feat: auth employee

---
+++ db/migrations/20230103084446_create_table_authentication.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE authentication (
+    token text not null
+)
+
+CREATE UNIQUE INDEX token_authentication ON authentication (token);
---
+++ internal/repositories/mysql_db/auth.go
@@ -0,0 +1,80 @@
+package mysql_db
+
+import (
+	"context"
+	"errors"
+
+	"github.com/elSyarif/posnote-api.git/internal/core/ports"
+	"github.com/jmoiron/sqlx"
+)
+
+type authRepository struct {
+	DB *sqlx.DB
+}
+
+func NewAuthRepository(connect *sqlx.DB) ports.AuthRepository {
+	return &authRepository{
+		DB: connect,
+	}
+}
+
+func (respository *authRepository) Save(ctx context.Context, token string) error {
+	query := "INSERT INTO authentication VALUES (?)"
+
+	tx, err := respository.DB.Beginx()
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	result, err := tx.ExecContext(ctx, query, token)
+	if err != nil {
+		tx.Rollback()
+		return errors.New(err.Error())
+	}
+
+	tx.Commit()
+	if row, _ := result.RowsAffected(); row < 0 {
+		return errors.New(err.Error())
+	}
+
+	return nil
+}
+
+func (respository *authRepository) VerifyRefreshToken(ctx context.Context, token string) error {
+	query := "SELECT token FROM authentication WHERE token = ?"
+
+	var jwt *string
+	tx, err := respository.DB.Beginx()
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	err = tx.GetContext(ctx, &jwt, query, token)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	return nil
+}
+
+func (respository *authRepository) DeleteRefreshToken(ctx context.Context, token string) error {
+	query := "DELETE FROM authentication WHERE token = ?"
+
+	tx, err := respository.DB.Beginx()
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	result, err := tx.ExecContext(ctx, query, token)
+	if err != nil {
+		tx.Rollback()
+		return errors.New(err.Error())
+	}
+
+	tx.Commit()
+	if row, _ := result.RowsAffected(); row < 0 {
+		return errors.New(err.Error())
+	}
+
+	return nil
+}
